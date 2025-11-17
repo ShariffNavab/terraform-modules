ac3ea2796eb177521045d689e17ee9c424f58832
Shariff Navab <shariff.navab@opstree.com>
2025-11-17 11:32:51 +0530

    creating new rg

--- environments/qa/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstate11775488"
-    container_name       = "tfstate"
-    key                  = "qa.terraform.tfstate"
-  }
-}
